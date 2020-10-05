37760cf1c4bb6ca66178f6990beaed1e631535f1
Ridha Ahmad Firdaus <ridhaaf@gmail.com>
2020-10-05 19:46:10 +0700

    Video #4

---
+++ app/Controllers/Coba.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controllers;
+
+class Coba extends BaseController
+{
+    public function index()
+    {
+        echo 'Ini controller Coba, method index';
+    }
+
+    public function about($nama = '')
+    {
+        echo "Halo, nama saya $nama.";
+    }
+
+    //--------------------------------------------------------------------
+
+}
