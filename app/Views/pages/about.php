980c76ccf26b431fe9d08914cf88058560ee8b7b
Ridha Ahmad Firdaus <ridhaaf@gmail.com>
2020-10-06 11:51:17 +0700

    Video #6

--- app/Views/pages/about.php
+++ app/Views/pages/about.php
@@ -1,8 +1,12 @@
-    <div class="container">
-        <div class="row">
-            <div class="col">
-                <h1>About Me</h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur enim provident, autem sint sapiente quae vitae incidunt alias ipsum consectetur laborum soluta dolore reiciendis totam saepe minima similique dolorum amet.</p>
-            </div>
+<?= $this->extend('layout/template'); ?>
+
+<?= $this->section('content'); ?>
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <h1>About Me</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur enim provident, autem sint sapiente quae vitae incidunt alias ipsum consectetur laborum soluta dolore reiciendis totam saepe minima similique dolorum amet.</p>
         </div>
     </div>
+</div>
+<?= $this->endSection(); ?>
